f1c11077fd5caf7766aa34454c0e58e50997e456
Anton <anton-fomin89@mail.ru>
2023-10-29 15:08:18 +0300

    1

--- home_8_10/resource.py
+++ home_8_10/resource.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-import home_8_10
+import tests
 
 
 def path(file_name):
     return str(
-        Path(home_8_10.__file__).parent.joinpath(f'picture/{file_name}').absolute()
+        Path(tests.__file__).parent.joinpath(f'picture/{file_name}').absolute()
     )
